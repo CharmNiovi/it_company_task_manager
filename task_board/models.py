5c6595bb8cca63048e3a2226003072a11eebf60a
charmniovi <charmniovi@tutanota.com>
2024-05-15 17:36:41 +0300

    added Position in models.py

--- task_board/models.py
+++ task_board/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class TaskType(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
