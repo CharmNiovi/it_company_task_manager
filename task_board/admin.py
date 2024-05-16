3cf2fc57f0d02492afc107046901446bdc7a5620
charmniovi <charmniovi@tutanota.com>
2024-05-16 09:49:01 +0300

    register Task model

--- task_board/admin.py
+++ task_board/admin.py
@@ -38,3 +38,16 @@ class ProjectAdmin(admin.ModelAdmin):
     fieldsets = (
         ("Info", {"fields": ("name", "team")}),
     )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("General info", {"fields": ("name", "description")}),
+        ("Tags", {"fields": ("tags",)}),
+        ("Time info", {"fields": ("priority", "deadline", "is_completed")}),
+        ("Specific info", {"fields": ("task_type", "project")})
+    )
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type", "project")
+    search_fields = ("name", "priority", "task_type")
+    list_filter = ("priority", "is_completed")
