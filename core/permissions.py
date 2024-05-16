9b4ba8af62dbe9ace48cfef007dc2e52a85a4e5a
charmniovi <charmniovi@tutanota.com>
2024-05-16 20:13:01 +0300

    sliced TeamStaffOrOwnerRequiredMixin on 3 other permission class: UserInTeamRequiredMixin, UserTeamOwnerRequiredMixin, UserTeamStaffRequiredMixin

--- core/permissions.py
+++ core/permissions.py
@@ -3,11 +3,29 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 
 
-class TeamStaffOrOwnerRequiredMixin(LoginRequiredMixin):
+class UserInTeamRequiredMixin(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.team_workers.filter(
-                Q(team_owner=True) | Q(team_staff=True),
-                team__projects__pk=kwargs['pk']
-        ):
+        if not self.check_authorization(request, **kwargs):
             raise Http404("You are not authorized to view this page")
         return super().dispatch(request, *args, **kwargs)
+
+    def check_authorization(self, request, **kwargs):
+        if request.user.is_anonymous:
+            return False
+        return request.user.team_workers.filter(
+                team__projects__pk=kwargs['pk']
+        ).exists()
+
+
+class UserTeamOwnerRequiredMixin(UserInTeamRequiredMixin):
+    def check_authorization(self, request, **kwargs):
+        if request.user.is_anonymous:
+            return False
+        return request.user.team_workers.filter(team_owner=True).exists()
+
+
+class UserTeamStaffRequiredMixin(UserInTeamRequiredMixin):
+    def check_authorization(self, request, **kwargs):
+        if request.user.is_anonymous:
+            return False
+        return request.user.team_workers.filter(team_staff=True).exists()
