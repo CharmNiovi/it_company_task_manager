cbd9e776c13197b39e6dee7b809dab0462eb7274
charmniovi <charmniovi@tutanota.com>
2024-05-16 09:46:17 +0300

    register Tag, TaskType, Position models

--- task_board/admin.py
+++ task_board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_board.models import TaskType, Position, Worker, Team, TeamWorker, Tag, Project, Task
+
+
+admin.site.register(Tag)
+admin.site.register(TaskType)
+admin.site.register(Position)
