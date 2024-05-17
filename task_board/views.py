09b314eae37ccb7696b25e5f91976303b305d24c
charmniovi <charmniovi@tutanota.com>
2024-05-17 16:30:54 +0300

    added ProjectListView

--- task_board/views.py
+++ task_board/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views import generic
 
-# Create your views here.
+from core.permissions import UserTeamOwnerRequiredMixin, TeamStaffOrOwnerRequiredMixin, UserInTeamRequiredMixin
+from task_board.forms import ProjectForm, TaskForm
+from task_board.models import Project, Task, TeamWorker, Team
+
+
+class ProjectListView(LoginRequiredMixin, generic.ListView):
+    def get_queryset(self):
+        return Project.objects.filter(team__worker=self.request.user).select_related("team")
+
