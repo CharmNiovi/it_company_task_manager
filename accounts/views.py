f64f7566c7327ea4b3906185d4b4c057c2291d24
charmniovi <charmniovi@tutanota.com>
2024-05-19 14:00:42 +0300

    deleted ResetPasswordView

--- accounts/views.py
+++ accounts/views.py
@@ -44,16 +44,3 @@ class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
     def get_object(self, queryset=None):
         print(self.request.user)
         return self.request.user
-
-# TODO: Зміна паролю
-
-
-class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'registration/password_reset.html'
-    email_template_name = 'registration/password_reset_email.html'
-    subject_template_name = 'registration/password_reset_subject'
-    success_message = "We've emailed you instructions for setting your password, " \
-                      "if an account exists with the email you entered. You should receive them shortly." \
-                      " If you don't receive an email, " \
-                      "please make sure you've entered the address you registered with, and check your spam folder."
-    success_url = reverse_lazy('users-home')
