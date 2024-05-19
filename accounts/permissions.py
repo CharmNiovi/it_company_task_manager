04ba33c01697debc48819f2d9229584021c16b84
charmniovi <charmniovi@tutanota.com>
2024-05-19 13:51:33 +0300

    added RequestedUserInSameTeamRequiredMixin in accounts

---
+++ accounts/permissions.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+
+from core.permissions import TeamWorkerBasedAccessMixin
+
+
+class RequestedUserInSameTeamRequiredMixin(TeamWorkerBasedAccessMixin):
+    """
+    Mixin class that checks if the requested user is in the same team as the profile user.
+    """
+
+    def filter_team_project_pk(self, user, *args, **kwargs):
+        if self.request.user.username != kwargs["username"]:
+            return user.filter(team__worker__username=kwargs["username"])
+        return get_user_model().objects.filter(username=self.request.user.username)
