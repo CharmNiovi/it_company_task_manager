c0e7d140b498f460f32ec6f60129e5eb82d2ff3a
charmniovi <charmniovi@tutanota.com>
2024-05-20 13:40:55 +0300

    added visualized models

--- core/settings.py
+++ core/settings.py
@@ -136,5 +136,6 @@ AUTH_USER_MODEL = "accounts.Worker"
 
 if DEBUG:
     INSTALLED_APPS += ["debug_toolbar"]
+    INSTALLED_APPS += ['django_extensions']
     MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
     INTERNAL_IPS = ["127.0.0.1"]
