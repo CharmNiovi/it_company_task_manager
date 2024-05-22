fc87838c3a7b958ef6863e6a71768a87e4a0e625
charmniovi <charmniovi@tutanota.com>
2024-05-22 10:37:57 +0300

    added media re_path

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,8 @@ from core import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
+from django.urls import re_path
 
 
 urlpatterns = [
@@ -25,10 +27,11 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
     path("", include("task_board.urls")),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
 
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
