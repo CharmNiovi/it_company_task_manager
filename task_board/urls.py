d8e7811e8a302fa384e8997218cf9c190f3498d7
charmniovi <charmniovi@tutanota.com>
2024-06-02 15:17:27 +0300

    added slash in url for TaskCreateView

--- task_board/urls.py
+++ task_board/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("project/<int:pk>/update/", ProjectUpdateView.as_view(), name="project-update"),
     path("project/<int:pk>/delete/", ProjectDeleteView.as_view(), name="project-delete"),
 
-    path("project/<int:pk>/task/create", TaskCreateView.as_view(), name="task-create"),
+    path("project/<int:pk>/task/create/", TaskCreateView.as_view(), name="task-create"),
     path(
         "project/<int:pk>/task/<int:task_pk>/update/",
         TaskUpdateView.as_view(),
